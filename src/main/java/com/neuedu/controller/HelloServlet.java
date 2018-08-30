a3687a200a63ad7f7a1212306d2d59424eeb334b
wttzbs <wttzbs@sina.com>
2018-08-31 00:10:27 +0800

    servlet讲解

--- src/main/java/com/neuedu/controller/HelloServlet.java
+++ src/main/java/com/neuedu/controller/HelloServlet.java
@@ -1,12 +1,20 @@
 package com.neuedu.controller;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 //基于注解
+//确切路径匹配http://localhost:8080/business/mng/hello
+//@WebServlet("/mng/hello")
+//确切路径匹配>模糊路径匹配>扩展名匹配>缺省的servlet
+//模糊路径匹配
+@WebServlet("/hello")//匹配任意路径/basic/*就不是任意了，必须是basic路径下的任意字符
+
 public class HelloServlet extends HttpServlet {
     public HelloServlet(){
         System.out.println("======构造方法===hello  servlet============");
@@ -32,7 +40,29 @@ public class HelloServlet extends HttpServlet {
         String username=request.getParameter("username");
         String password=request.getParameter("password");
         System.out.println(username);
-
+        request.setAttribute("username","admin");
+        //请求分派
+//        request.getRequestDispatcher("login.jsp").forward(request,response);
+      /*
+      servlet作用域：请求域，  会话域，  全局域
+       */
+      //会话域
+        HttpSession httpSession=request.getSession();
+        httpSession.setAttribute("username","admin");
+        //统计访问Helloservlet的次数
+        ServletContext application=this.getServletContext();
+        Object count=application.getAttribute("count");
+        if(count==null){
+            application.setAttribute("count",1);
+        }else{
+            int _count=(int)count;
+            application.setAttribute("count",++_count);
+        }
+        System.out.println("访问次数"+application.getAttribute("count"));
+//      request.setAttribute("username","admin");
+//      //往请求域存放数据key-value形式存放
+        //重定向
+        response.sendRedirect("login.jsp");
     }
 
     @Override
---
+++ target/Business/login.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>HAHAHAHA ${username}</h2>
+</body>
+</html>
